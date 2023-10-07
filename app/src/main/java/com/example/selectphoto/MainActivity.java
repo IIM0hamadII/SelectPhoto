a341354dea5f3b793eba9e04be951d4f658b6dea
IIM0hamadII <hamoudy1221h@gmail.com>
2023-10-07 08:14:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/selectphoto/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.selectphoto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnimg;
+
+    ImageView Previewimage;
+
+    int SELECT_PICTURE = 200;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnimg=findViewById(R.id.btnimg);
+        Previewimage=findViewById(R.id.Previewimage);
+        btnimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageChooser();
+            }
+        });
+
+
+    }
+    public void ImageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(i,"SELECT PICTURE"),SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        Uri SIuri = data.getData();
+        Previewimage.setImageURI(SIuri);
+    }
+}
